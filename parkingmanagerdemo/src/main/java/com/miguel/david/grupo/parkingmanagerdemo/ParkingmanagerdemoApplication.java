4c1af059cab92f08f1fe21c1deae315eac2fd226
David Ortiz Corchero <david140303@hotmail.es>
2023-01-18 19:55:16 +0100

    Primer push

---
+++ parkingmanagerdemo/src/main/java/com/miguel/david/grupo/parkingmanagerdemo/ParkingmanagerdemoApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.david.grupo.parkingmanagerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingmanagerdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingmanagerdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguel/david/grupo/parkingmanagerdemo/ParkingmanagerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.david.grupo.parkingmanagerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingmanagerdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

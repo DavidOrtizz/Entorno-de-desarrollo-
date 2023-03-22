89b8c6ced42ed6fdacd5f730472c1f48927c17b3
David Ortiz Corchero <david140303@hotmail.es>
2023-03-22 19:52:13 +0100

    Creacion

---
+++ src/main/java/com/miguel/david/grupo/parkingmanagerdemo/registro/service/RegistroService.java
@@ -0,0 +1,9 @@
+package com.miguel.david.grupo.parkingmanagerdemo.registro.service;
+
+public class RegistroService {
+
+  public Object getAll() {
+    return null;
+  }
+  
+}
---
+++ src/main/java/com/miguel/david/grupo/parkingmanagerdemo/registro/service/RegistroServiceImpl.java
@@ -0,0 +1,5 @@
+package com.miguel.david.grupo.parkingmanagerdemo.registro.service;
+
+public class RegistroServiceImpl {
+  
+}

abd204829c1a889145b4902dcaac9cf37a0c7f0d
David Ortiz Corchero <david140303@hotmail.es>
2023-04-26 22:20:11 +0200

    Arreglo

--- src/main/java/com/miguel/david/grupo/parkingmanagerdemo/user/domain/UserDao.java
+++ src/main/java/com/miguel/david/grupo/parkingmanagerdemo/user/domain/UserDao.java
@@ -14,9 +14,11 @@ public class UserDao {
     private String apellido1;
     private String apellido2;
     private Rol rol;
-    public UserDao(){
-        this("","","","",null);
+
+    public UserDao() {
+        this("", "", "", "", null);
     }
+
     public UserDao(String email, String nombre, String apellido1, String apellido2, Rol rol) {
         this.email = email;
         this.nombre = nombre;
@@ -24,34 +26,44 @@ public class UserDao {
         this.apellido2 = apellido2;
         this.rol = rol;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getApellido1() {
         return apellido1;
     }
+
     public void setApellido(String apellido1) {
         this.apellido1 = apellido1;
     }
+
     public String getApellido2() {
         return apellido2;
     }
+
     public void setApellido2(String apellido2) {
         this.apellido2 = apellido2;
     }
+
     public Rol getRole() {
         return rol;
     }
-    public void setRole(Rol role) {
+
+    public void setRole(Rol rol) {
         this.rol = rol;
     }
 }

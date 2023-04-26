223de61c669d4d7bd29af0c05012b7215eebcf04
Michel-2004 <mvilbou1403@g.educaand.es>
2023-04-26 19:38:20 +0200

    añadir exception

--- src/main/java/com/miguel/david/grupo/parkingmanagerdemo/user/domain/User.java
+++ src/main/java/com/miguel/david/grupo/parkingmanagerdemo/user/domain/User.java
@@ -3,6 +3,7 @@ package com.miguel.david.grupo.parkingmanagerdemo.user.domain;
 
 import java.util.Set;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,8 +23,10 @@ import jakarta.persistence.ManyToMany;
 public class User {
   // Atributos que varían para cada objeto
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(unique = true)
+  private String email;
   private String nombre;
   private String apellido1;
   private String apellido2;
